44bc3890ad950539ba0dafa64eca9389b48c1956
saml <skynare@gmail.com>
2013-04-01 22:54:22 -0400

    hola

--- pebl/__init__.py
+++ pebl/__init__.py
@@ -1,5 +1,34 @@
+import yaml
 
-class Content(object):
+
+class YamlFrontMatterParser(object):
+	def __init__(self, file_like):
+		self.file_like = file_like
+		self.curr_state = self.Start
+		self.yaml_lines = []
+		self.body_lines = []
+		for line in file_like:
+			self.curr_state(line)
+		self.yaml = yaml.safe_load(''.join(self.yaml_lines)) or {}
+		self.body = ''.join(self.body_lines)
+		
+
+	def Start(self, line):
+		if line == '---\n':
+			self.curr_state = self.YamlState
+		elif line != '\n':
+			self.curr_state = self.BodyState
+
+	def YamlState(self, line):
+		if line == '---\n':
+			self.curr_state = self.BodyState
+		else:
+			self.yaml_lines.append(line)
+
+	def BodyState(self, line):
+		self.body_lines.append(line)
+
+class Content_(object):
 	def __init__(self, path, ext='.html', title='', content=None, prefix='/content/',
 		last_modified=None, created=None):
 		self.path = path
--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 import bottle
 import misaka
 
-import settings
+import os
 
+import settings
+from pebl import YamlFrontMatterParser
 
 bottle.debug(settings.DEBUG)
 
@@ -15,6 +17,25 @@ def render_template(name, **kwargs):
 @bottle.route('/admin/<path:path>')
 def index(path):
     return render_template('creator.html')
+
+@bottle.route('/posts/<path:path>')
+def blog_entry(path):
+	entry_id,ext = os.path.splitext(path)
+	content_path = os.path.join(settings.CONTENT_DIR, entry_id)
+	content_path_html = content_path + '.html'
+	content_path_markdown = content_path + '.md'
+	if os.path.exists(content_path_html):
+		with open(content_path_html, 'r') as f:
+			yaml_front_matter = YamlFrontMatterParser(f)
+			return render_template('blog_entry.html', 
+				metadata=yaml_front_matter.yaml, body=yaml_front_matter.body)
+	elif os.path.exists(content_path_markdown):
+		with open(content_path_markdown, 'r') as f:
+			yaml_front_matter = YamlFrontMatterParser(f)
+			return render_template('blog_entry.html',
+				metadata=yaml_front_matter.yaml, body=misaka.html(yaml_front_matter.body))
+	else:
+		bottle.abort(404, 'Not Found')
 
 
 if __name__ == '__main__':
